ade72c14d8222bd362d49ea79882b323014c129e
MatthieuGalante <Matthieu.galante5@etu.univ-lorraine.fr>
2021-01-01 15:28:47 +0100

    ajout des routes du système d'item

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use \mywishlist\controls\ListeControleur as ListeControleur;
 use \mywishlist\controls\PageControleur as PageControleur;
+use \mywishlist\controls\ItemControleur as ItemControleur;
 
 use Slim\Flash\Messages;
 
@@ -46,6 +47,21 @@ $app->get('/liste/{tokenPublic:[a-zA-Z0-9]+}/edit/{tokenPrivate:[a-zA-Z0-9]+}', 
 $app->post('/liste/{tokenPublic:[a-zA-Z0-9]+}/edit/{tokenPrivate:[a-zA-Z0-9]+}', ListeControleur::class.':updateListe')->setName('edition_liste_post');
 
 $app->post('/liste/{tokenPublic:[a-zA-Z0-9]+}/addMessage', ListeControleur::class.':addMessage')->setName('add_message_post');
+
+
+$app->get('/liste/{tokenPublic]/item/{idItem}', ItemControleur::class.':getItem')->setName('affichage_item');
+
+$app->post('/liste/{tokenPublic]/item/{idItem}/reserve', ItemControleur::class.':reserverItem')->setName('reservation_item');
+
+$app->post('/liste/{tokenPublic}/edit/{tokenPrivate}/item/{idItem}/delete', ItemControleur::class.':deleteItem')->setName('suppression_item');
+
+$app->get('/liste/{tokenPublic}/edit/{tokenPrivate}/item/add ', ItemControleur::class.':createItem')->setName('créer_item');
+
+$app->post('/liste/{tokenPublic}/edit/{tokenPrivate}/item/add ->', ItemControleur::class.':insertItem')->setName('créer_item_post');
+
+$app->get('/liste/{tokenPublic}/edit/{tokenPrivate}/item/{idItem}/edit', ItemControleur::class.':editItem')->setName('edition_item');
+
+$app->post('/liste/{tokenPublic}/edit/{tokenPrivate}/item/{idItem}/edit ', ItemControleur::class.':updateItem')->setName('edition_item_post');
 
 // Lancement de l'application
 $app->run();
