8a4663fc493bf8b4824e3c9a26419d2666a04429
MatthieuGalante <Matthieu.galante5@etu.univ-lorraine.fr>
2021-01-02 14:56:57 +0100

    ajout editItem dans ItemControleur

--- src/controls/ItemControleur.php
+++ src/controls/ItemControleur.php
@@ -66,6 +66,44 @@ class ListeControleur
 
     }
 
+    public function editItem(Request $rq, Response $rs, $args)
+    {
+        $tokenPublic = $args['tokenPublic'];
+        $tokenPrivate = $args['tokenPrivate'];
+
+        $liste = Liste::where('token', '=', $tokenPublic)->first();
+
+        if (is_null($liste)) {
+            $this->app->flash->addMessage('Alerte', 'La liste n\'existe pas !');
+            return $rs->withRedirect($this->app->router->pathFor('accueil'));
+        } else if (!$liste->isEditable($tokenPrivate)) {
+            $this->app->flash->addMessage('Alerte', 'Le token de modification est invalide !');
+            return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+        } else if ($liste->isExpired()) {
+            $this->app->flash->addMessage('Alerte', 'Il n\'est pas possible de modifier une liste expirée !');
+            return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+        }
+
+        $id = $args['id'];
+
+        $item = Item::where(['id' => $id, 'liste_id' => $liste->id])->first();
+
+        if (is_null($item)) {
+            $this->app->flash->addMessage('Alerte', 'L item n\'existe pas !');
+            return $rs->withRedirect($this->app->router->pathFor('accueil'));
+        }
+
+        if ($item->isReserved() == true) {
+            $this->app->flash->addMessage('Alerte', 'L item est déjà réservé !');
+            return $rs->withRedirect($this->app->router->pathFor('accueil'));
+        }
+
+
+        /**affichage de la vue**/
+        return $rs;
+
+    }
+
 }
 
 ?>
