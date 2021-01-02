212307e08ac1f33e4fcc030337d9070e0b8ca20b
MatthieuGalante <Matthieu.galante5@etu.univ-lorraine.fr>
2021-01-02 15:07:33 +0100

    ajout reserverItem dans ItemControleur

--- src/controls/ItemControleur.php
+++ src/controls/ItemControleur.php
@@ -104,6 +104,65 @@ class ListeControleur
 
     }
 
+    public function reserverItem(Request $rq, Response $rs, $args)
+    {
+        $tokenPublic = $args['tokenPublic'];
+
+        $liste = Liste::where('token', '=', $tokenPublic)->first();
+
+        if (is_null($liste)) {
+            $this->app->flash->addMessage('Alerte', 'La liste n\'existe pas !');
+            return $rs->withRedirect($this->app->router->pathFor('accueil'));
+        }  else if ($liste->isExpired()) {
+            $this->app->flash->addMessage('Alerte', 'Il n\'est pas possible de modifier une liste expirée !');
+            return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+        }
+
+        $id = $args['id'];
+
+        $item = Item::where(['id' => $id, 'liste_id' => $liste->id])->first();
+
+        if (is_null($item)) {
+            $this->app->flash->addMessage('Alerte', 'L item n\'existe pas !');
+            return $rs->withRedirect($this->app->router->pathFor('accueil'));
+        }
+
+        if ($item->isReserved() == true) {
+            $this->app->flash->addMessage('Alerte', 'L item est déjà réservé !');
+            return $rs->withRedirect($this->app->router->pathFor('accueil'));
+        }
+
+
+
+        $post = $rq->getParsedBody() ;
+        $message = filter_var($post['message'], FILTER_SANITIZE_STRING) ;
+        $identite = filter_var($post['identite'] , FILTER_SANITIZE_STRING);
+
+        if(strlen($message) < 10) {
+            $this->app->flash->addMessage('Alerte', 'Le message doit au moins faire 10 caractères !');
+        } else if(strlen($identite) < 5) {
+            $this->app->flash->addMessage('Alerte', 'Votre pseudo doit faire au moins 5 caractères !');
+        }  else {
+            $reserv = new Reservation();
+            $reserv->nom = $identite;
+            $reserv->message = $message;
+            $reserv->id_item = $item->id;
+            $reserv->date = new DateTime();
+            $reserv->save();
+
+            // Création cookie identité
+            setcookie("username", serialize ($identite),time() + 60*60*24*365*10, "/" ) ;
+
+            $this->app->flash->addMessage('Ok', 'Le message a été ajouté.');
+        }
+
+
+        /**affichage de la vue**/
+
+        return $rs;
+
+    }
+
 }
 
 ?>
