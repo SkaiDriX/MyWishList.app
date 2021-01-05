6b170bad5972ac2e85ee68f414383d61a8d913a6
Victor NOEL <victor.noel2@etu.univ-lorraine.fr>
2021-01-05 14:42:29 +0100

    Ajout methodes deleteItem, insertItem, updateItem

--- src/controls/ItemControleur.php
+++ src/controls/ItemControleur.php
@@ -43,7 +43,7 @@ class ItemControleur
         $data['item'] = $item;
 
         // On récupère la réservation sur l'item si elle existe
-        $data['reserved'] = $item->isReserved(); 
+        $data['reserved'] = $item->isReserved();
         if($data['reserved']) {
             $data['reservation'] = $item->reservation;
         }
@@ -174,11 +174,131 @@ class ItemControleur
 
             $this->app->flash->addMessage('Ok', 'Vous avez réserver l\'objet !');
         }
-        
+
         return $rs->withRedirect( $this->app->router->pathFor('affichage_item', ['tokenPublic' => $tokenPublic, 'idItem' => $item->id]));
 
     }
 
+    public function deleteItem(Request $rq, Response $rs, $args)
+    {
+      $tokenPublic = $args['tokenPublic'];
+      $tokenPrivate = $args['tokenPrivate'];
+
+      $liste = Liste::where('token', '=', $tokenPublic)->first();
+
+      if (is_null($liste)) {
+          $this->app->flash->addMessage('Alerte', 'La liste n\'existe pas !');
+          return $rs->withRedirect($this->app->router->pathFor('accueil'));
+      } else if (!$liste->isEditable($tokenPrivate)) {
+          $this->app->flash->addMessage('Alerte', 'Le token de modification est invalide !');
+          return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+      } else if ($liste->isExpired()) {
+          $this->app->flash->addMessage('Alerte', 'Il n\'est pas possible de modifier une liste expirée !');
+          return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+      }
+
+      $id = $args['idItem'];
+      $item = Item::where(['id' => $id, 'liste_id' => $liste->id])->first();
+
+      if (is_null($item)) {
+          $this->app->flash->addMessage('Alerte', 'L item n\'existe pas !');
+          return $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));
+      }
+
+      if ($item->isReserved() == true) {
+          $this->app->flash->addMessage('Alerte', 'L item est déjà réservé !');
+          return $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));
+      }
+
+      $item->delete();
+
+      $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));))
+    }
+
+    public function insertItem(Request $rq, Response $rs, $args)
+    {
+      $tokenPublic = $args['tokenPublic'];
+      $tokenPrivate = $args['tokenPrivate'];
+
+      $liste = Liste::where('token', '=', $tokenPublic)->first();
+
+      if (is_null($liste)) {
+          $this->app->flash->addMessage('Alerte', 'La liste n\'existe pas !');
+          return $rs->withRedirect($this->app->router->pathFor('accueil'));
+      } else if (!$liste->isEditable($tokenPrivate)) {
+          $this->app->flash->addMessage('Alerte', 'Le token de modification est invalide !');
+          return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+      } else if ($liste->isExpired()) {
+          $this->app->flash->addMessage('Alerte', 'Il n\'est pas possible de modifier une liste expirée !');
+          return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+      }
+    $post = $rq->getParsedBody() ;
+    $titre = filter_var($post['titre'], FILTER_SANITIZE_STRING) ;
+    $desc = filter_var($post['desc'] , FILTER_SANITIZE_STRING);
+    $url = filter_var($post['url'] , FILTER_SANITIZE_STRING);
+    $img = filter_var($post['img'] , FILTER_SANITIZE_STRING);
+    $tarif = filter_var($post['tarif'] , FILTER_SANITIZE_STRING);
+
+    $item = new Item();
+    $item->liste_id = $liste->id;
+    $item->nom = $titre;
+    $item->descr = $desc;
+    $item->img = $img;
+    $item->url = $url;
+    $item->tarif = $tarif;
+    $item->save();
+
+    return $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));
+    }
+
+    public function updateItem(Request $rq, Response $rs, $args)
+    {
+      $tokenPublic = $args['tokenPublic'];
+      $tokenPrivate = $args['tokenPrivate'];
+
+      $liste = Liste::where('token', '=', $tokenPublic)->first();
+
+      if (is_null($liste)) {
+          $this->app->flash->addMessage('Alerte', 'La liste n\'existe pas !');
+          return $rs->withRedirect($this->app->router->pathFor('accueil'));
+      } else if (!$liste->isEditable($tokenPrivate)) {
+          $this->app->flash->addMessage('Alerte', 'Le token de modification est invalide !');
+          return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+      } else if ($liste->isExpired()) {
+          $this->app->flash->addMessage('Alerte', 'Il n\'est pas possible de modifier une liste expirée !');
+          return $rs->withRedirect($this->app->router->pathFor('affichage_liste', ['tokenPublic' => $tokenPublic]));
+      }
+
+      $id = $args['idItem'];
+      $item = Item::where(['id' => $id, 'liste_id' => $liste->id])->first();
+
+      if (is_null($item)) {
+          $this->app->flash->addMessage('Alerte', 'L item n\'existe pas !');
+          return $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));
+      }
+
+      if ($item->isReserved() == true) {
+          $this->app->flash->addMessage('Alerte', 'L item est déjà réservé !');
+          return $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));
+      }
+
+    $post = $rq->getParsedBody() ;
+    $titre = filter_var($post['titre'], FILTER_SANITIZE_STRING) ;
+    $desc = filter_var($post['desc'] , FILTER_SANITIZE_STRING);
+    $url = filter_var($post['url'] , FILTER_SANITIZE_STRING);
+    $img = filter_var($post['img'] , FILTER_SANITIZE_STRING);
+    $tarif = filter_var($post['tarif'] , FILTER_SANITIZE_STRING);
+
+    $item->liste_id = $liste->id;
+    $item->nom = $titre;
+    $item->descr = $desc;
+    $item->img = $img;
+    $item->url = $url;
+    $item->tarif = $tarif;
+    $item->save();
+
+    return $rs->withRedirect($this->app->router->pathFor('edition_liste',[ 'tokenPublic' => $tokenPublic, 'tokenPrivate' => $tokenPrivate]));
+    }
 }
 
 ?>
